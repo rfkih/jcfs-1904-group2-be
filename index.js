ab03c8e6366d115a5c0973740471e594183314c1
rochafialvin <rochafi.dev@gmail.com>
2022-04-07 23:44:44 +0700

    Use API_PORT env

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const path = require('path')
-const port = 2021;
+const path = require("path");
+const port = process.env.API_PORT;
 
-const stocksRouter = require("./src/routers/stocks")
-const userRouter = require("./src/routers/users")
-const productRouter = require ("./src/routers/products")
-const categoriesRouter = require ("./src/routers/categories")
-const transactionRouter = require ("./src/routers/transaction")
-const transactiondetailRouter = require("./src/routers/transactionDetails")
+const stocksRouter = require("./src/routers/stocks");
+const userRouter = require("./src/routers/users");
+const productRouter = require("./src/routers/products");
+const categoriesRouter = require("./src/routers/categories");
+const transactionRouter = require("./src/routers/transaction");
+const transactiondetailRouter = require("./src/routers/transactionDetails");
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,11 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/transaction", transactionRouter)
-app.use("/transactiondetails", transactiondetailRouter)
-app.use("/stocks", stocksRouter)
-app.use("/categories", categoriesRouter)
-app.use("/products", productRouter)
+app.use("/transaction", transactionRouter);
+app.use("/transactiondetails", transactiondetailRouter);
+app.use("/stocks", stocksRouter);
+app.use("/categories", categoriesRouter);
+app.use("/products", productRouter);
 app.use("/users", userRouter);
 app.get("/", (req, res) => {
   res.status(200).send("API IS RUNNING");
