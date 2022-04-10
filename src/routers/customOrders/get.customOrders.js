1c3cbec89988c57316a210dce0a52cc09f2090a2
rfkih <rifkihidayat23@yahoo.com>
2022-04-10 20:51:25 +0700

    sunday night

--- src/routers/customOrders/get.customOrders.js
+++ src/routers/customOrders/get.customOrders.js
@@ -21,10 +21,30 @@ const getCustomOrderRouter =  async (req, res, next) => {
   };
 
 
+  const getCustomOrderByIdRouter =  async (req, res, next) => {
+    try {
+        const connection = await mysql2.promise().getConnection()
+
+
+  
+      const sqlGetOrder = `select * from custom_order where id = ?;`;
+      
+  
+      const [result] = await connection.query(sqlGetOrder, req.params.orderId );
+     
+      connection.release();
+  
+      res.status(200).send(result);
+    } catch (error) {
+      next(error)
+    }
+  };
 
 
 
 
+
+  router.get("/:orderId", getCustomOrderByIdRouter )
 router.get("/", getCustomOrderRouter )
 
 module.exports = router;
