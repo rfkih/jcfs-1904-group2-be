72b133478d958dc86406e97c62c0498a97e2d13b
rfkih <rifkihidayat23@yahoo.com>
2022-04-20 16:59:50 +0700

    pagination data

--- src/routers/dataLog/get.dataLog.js
+++ src/routers/dataLog/get.dataLog.js
@@ -5,8 +5,7 @@ const pool = require("../../config/database");
 const getDataLogRouter = async (req, res, next) => {
     try {
       const connection = await pool.promise().getConnection();
-      console.log(req.query.filterData);
-      const sqlGetDataLog = `select products.id, products.productName, data_logging.stock_in, data_logging.stock_out, data_logging.status, data_logging.created_at, data_logging.username from products inner join data_logging on products.id=product_id ${req.query.filterData} ${req.query.sortData}`;
+      const sqlGetDataLog = `select products.id, products.productName, data_logging.stock_in, data_logging.stock_out, data_logging.status, data_logging.created_at, data_logging.username from products inner join data_logging on products.id=product_id ${req.query.filterData} ${req.query.sortData} ${req.query.pages}`;
       const sqlCountDataLog = `SELECT COUNT(*) AS count FROM data_logging `;
   
       const [result] = await connection.query(sqlGetDataLog);
