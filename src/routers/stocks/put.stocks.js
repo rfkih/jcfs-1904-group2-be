d9a98a35d21eb29190c7a4c3aa23164a755b6787
rfkih <rifkihidayat23@yahoo.com>
2022-04-22 11:07:47 +0700

    add product

--- src/routers/stocks/put.stocks.js
+++ src/routers/stocks/put.stocks.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const pool = require("../../config/database");
 
+
+
+////update product router
+
 const putUpdateStocksRouter = async (req, res, next) => {
  
-
     try {
         const connection = await pool.promise().getConnection()
        
@@ -20,11 +23,9 @@ const putUpdateStocksRouter = async (req, res, next) => {
             }
 
             let writtenStock = 0
-
             
                 writtenStock = Math.abs(newStock - stock)
-           
-            
+                
             const sqlPostLog = "INSERT INTO data_logging SET ?"
 
             const dataLogIn = [{
@@ -65,11 +66,56 @@ const putUpdateStocksRouter = async (req, res, next) => {
         next(error);
     }
   } catch (error) {
-    connection.rollback();
+  
     next(error);
   }
 };
 
+
+//add update stock
+
+const putAddStocksRouter = async (req, res, next) => {
+ 
+    try {
+        const connection = await pool.promise().getConnection()
+      
+        try {
+                
+            const sqlPostLog = "INSERT INTO data_logging SET ?"
+
+            const dataLogIn = [{
+                user_id: req.body.userId,
+                username: req.body.username,
+                product_id: req.body.params.id,
+                stock_in: req.body.calculatedAddStock,
+                status: 'add',
+                }]
+
+                await connection.query(sqlPostLog, dataLogIn) 
+
+            const sqlUpdateProduct = `UPDATE stocks SET ? WHERE product_id = ?`;
+
+            const dataUpdateProduct = [req.body.updatedStocks, req.body.params.id]
+            const result =  await connection.query(sqlUpdateProduct, dataUpdateProduct) 
+           
+            res.status(201).send({
+                message: `Add Stock Success`,
+                
+            });
+     
+    } catch (error) {
+        
+        next(error);
+    }
+  } catch (error) {
+   
+    next(error);
+  }
+};
+
+
+
+router.put("/add/:productsId", putAddStocksRouter)
 router.put("/:productsId", putUpdateStocksRouter);
 
 module.exports = router;
