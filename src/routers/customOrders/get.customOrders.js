13eafcd0b06bb1ee4c53e99541ec3658a5ed5e3c
rfkih <rifkihidayat23@yahoo.com>
2022-04-22 20:38:15 +0700

    custom order

--- src/routers/customOrders/get.customOrders.js
+++ src/routers/customOrders/get.customOrders.js
@@ -24,6 +24,9 @@ const getCustomOrderRouter =  async (req, res, next) => {
   const getCustomOrderByIdRouter =  async (req, res, next) => {
     try {
         const connection = await pool.promise().getConnection()
+
+      console.log(req.query);
+      console.log(req.params);
   
       const sqlGetOrder = `select * from custom_order where id = ${req.params.orderId};`;
       
@@ -39,9 +42,31 @@ const getCustomOrderRouter =  async (req, res, next) => {
   };
 
 
+  const getCustomOrderByUserIdRouter =  async (req, res, next) => {
+    try {
+        const connection = await pool.promise().getConnection()
+
+    
+  
+      const sqlGetOrder = `select * from custom_order where user_id = ${req.params.userId};`;
+      
+  
+      const [result] = await connection.query(sqlGetOrder );
+      console.log(result);
+      connection.release();
+  
+      res.status(200).send(result);
+    } catch (error) {
+      next(error)
+    }
+  };
 
 
 
+
+
+
+router.get("/user/:userId", getCustomOrderByUserIdRouter)
   router.get("/:orderId", getCustomOrderByIdRouter )
 router.get("/", getCustomOrderRouter )
 
--- src/routers/customOrders/index.js
+++ src/routers/customOrders/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+
+const postCustomOrderRouter = require("./post.customOrders")
 const getCustomOrderRouter = require("./get.customOrders")
 const putCustomOrderRouter = require("./put.customOrders")
 
@@ -9,5 +11,6 @@ const putCustomOrderRouter = require("./put.customOrders")
 
 router.use(getCustomOrderRouter)
 router.use(putCustomOrderRouter)
+router.use(postCustomOrderRouter)
 
 module.exports = router;
