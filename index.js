07c94195f54fc16a2d88aefff0d349015e8eb429
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-05-23 15:29:56 +0700

    tes action

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const categoriesRouter = require("./src/routers/categories");
 const transactionRouter = require("./src/routers/transaction");
 const transactiondetailRouter = require("./src/routers/transactionDetails");
 const customordersRouter = require("./src/routers/customOrders/");
-const dataLogRouter = require("./src/routers/dataLog")
-const cartRouter = require("./src/routers/cart")
-const addressRouter = require("./src/routers/address")
-const paymentRouter = require("./src/routers/payment")
-const rajaOngkirRouter = require("./src/routers/rajaOngkir")
+const dataLogRouter = require("./src/routers/dataLog");
+const cartRouter = require("./src/routers/cart");
+const addressRouter = require("./src/routers/address");
+const paymentRouter = require("./src/routers/payment");
+const rajaOngkirRouter = require("./src/routers/rajaOngkir");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,19 +26,18 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use("/rajaongkir", rajaOngkirRouter )
+app.use("/rajaongkir", rajaOngkirRouter);
 app.use("/transaction", transactionRouter);
-app.use("/payment", paymentRouter)
+app.use("/payment", paymentRouter);
 app.use("/customorders", customordersRouter);
 app.use("/transactiondetails", transactiondetailRouter);
 app.use("/stocks", stocksRouter);
 app.use("/categories", categoriesRouter);
 app.use("/products", productRouter);
-app.use("/datalog", dataLogRouter)
+app.use("/datalog", dataLogRouter);
 app.use("/users", userRouter);
-app.use("/cart", cartRouter)
-app.use("/address", addressRouter)
+app.use("/cart", cartRouter);
+app.use("/address", addressRouter);
 app.get("/", (req, res) => {
   res.status(200).send("API IS RUNNING");
 });
@@ -55,5 +54,5 @@ app.use((error, req, res, next) => {
 app.listen(port, (err) => {
   if (err) return cosole.log({ err });
 
-  console.log(`API IS RUNNING AT PORT ${port}`);
+  console.log(`API IS RUNNING AT PORT ${port} 🚀`);
 });
